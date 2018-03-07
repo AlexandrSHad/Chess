9343374fdec0c0f61f0dcec0fe630d0c8f05c4bc
Alexandr SHad <shadurskij@gmail.com>
2018-03-07 16:10:50 +0200

    added Color and Figure enums, Square, FigureOnSquare, FigureMoving, Board classes

--- Chess/Chess.cs
+++ Chess/Chess.cs
@@ -9,21 +9,32 @@ namespace Chess
     public class Chess
     {
         public string fen { get; private set; }
+        Board board;
 
         public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
         {
             this.fen = fen;
+            board = new Board(fen);
+        }
+
+        Chess(Board board)
+        {
+            this.board = board;
         }
 
         public Chess Move(string move) // Pe2e4, Pe7e8Q
         {
-            var nextChess = new Chess(fen);
+            var fm = new FigureMoving(move);
+            var nextBoard = board.Move(fm);
+            var nextChess = new Chess(nextBoard);
             return nextChess;
         }
 
         public Char GetFigureAt(int x, int y)
         {
-            return '.';
+            var square = new Square(x, y);
+            var figure = board.GetFigureAt(square);
+            return figure == Figure.none ? '.' : (Char)figure;
         }
     }
 }
---
+++ Chess/Figure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    enum Figure
+    {
+        none,
+
+        whiteKing = 'K',
+        whiteQueen = 'Q',
+        whiteRook = 'R',
+        whiteBishop = 'B',
+        whiteKnife = 'N',
+        whitePawn = 'P',
+
+        blackKing = 'k',
+        blackQueen = 'q',
+        blackRook = 'r',
+        blackBishop = 'b',
+        blackKnife = 'n',
+        blackPawn = 'p'
+    }
+}
