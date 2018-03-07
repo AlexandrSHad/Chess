0b54fbef874fe01a5fe41d09a67138440a2cc037
Alexandr SHad <shadurskij@gmail.com>
2018-03-07 14:30:59 +0200

    demo chess druft

---
+++ DemoChess/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chess = new Chess.Chess();
+
+            while (true)
+            {
+                Console.WriteLine(chess.fen);
+                var move = Console.ReadLine();
+                if (move == "") break;
+                chess = chess.Move(move);
+            }
+        }
+    }
+}
